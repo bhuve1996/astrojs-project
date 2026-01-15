cefd294f1caa9509a2eadac64a5f1591b74be892
bhuve1996 <bhuve1996@gmail.com>
2026-01-15 17:36:17 +0530

    Remove all project files, keep only .gitignore

--- src/utils/client/init.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Client-side initialization script
- * This file is imported in Layout.astro
- */
-
-import { initInteractiveComponents } from './index';
-
-// Initialize when script loads
-initInteractiveComponents();
